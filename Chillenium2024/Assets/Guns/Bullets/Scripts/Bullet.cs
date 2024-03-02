7f9c750b6e821488b8e6892fb66893f55deed4dc
SmileCoder1 <venterap@gmail.com>
2024-03-01 23:52:11 -0600

    lines lul

---
+++ Chillenium2024/Assets/Guns/Bullets/Scripts/Bullet.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public static class UsefulConsts
+{
+    public static float degreeToRad = 0.0174532925199f;
+    public static float radToDegree = 57.2957795131f;
+    public static Vector2 unitVecFromAngle(float deg)
+    {
+        return new Vector2(Mathf.Cos(deg * UsefulConsts.degreeToRad), Mathf.Sin(deg * UsefulConsts.degreeToRad));
+    }
+}
+
+
+public class Bullet : MonoBehaviour
+{
+
+    public float maxDist = 10f;
+    public List<string> bounceableTags = new List<string>(new[] { "Wall" });
+    public List<string> termTags = new List<string>(new[] { "Enemy" });
+
+    public virtual void preComp()
+    {
+        Debug.Log("preComp Not Defined");
+        return;
+    }
+
+
+    public virtual void handleGunShot(Vector2 start, float dir)
+    {
+        Debug.Log("handleGunShot Not Defined");
+        return;
+
+    }
+
+    public virtual void renderGunShot()
+    {
+        Debug.Log("Render Not Defined");
+        return;
+    }
+
+    public virtual void shoot(Vector2 start, float dir)
+    {
+        handleGunShot(start, dir);
+    }
+
+    
+
+    
+
+    
+
+}
---
+++ Chillenium2024/Assets/Guns/Scripts/GunBehaviorScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunBehaviorScript : MonoBehaviour
+{
+
+    public float cooldown = 0.5f;
+    public float timeSinceShot = 0;
+    public float force = 10;
+    public bool mousePressed = false;
+    public Bullet bulletType;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            shootReq();
+    }
+
+    private void FixedUpdate()
+    {
+        timeSinceShot += Time.fixedDeltaTime;
+        
+    }
+
+
+    void shootReq()
+    {
+        if(timeSinceShot >= cooldown)
+        {
+            timeSinceShot = 0;
+            Bullet bullet = Instantiate(bulletType);
+            bullet.shoot(transform.position, gameObject.transform.eulerAngles.z + 90); //idk why we need the 90 but it doesn't work without it
+        }
+
+    }
+    
+}
