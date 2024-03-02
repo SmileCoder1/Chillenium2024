0f8af5dfc80900ae2327475e1f96222c2e7b07d6
SmileCoder1 <venterap@gmail.com>
2024-03-02 17:07:39 -0600

    commiting to pull

--- Chillenium2024/Assets/Roper/Roper.cs
+++ Chillenium2024/Assets/Roper/Roper.cs
@@ -89,13 +89,12 @@ public class Roper : MonoBehaviour
 
         //RaycastHit2D hit = Physics2D.Raycast(monke, dir, 1000f, 1 << LayerMask.NameToLayer("Wall"));
         RaycastHit2D hit = Physics2D.Raycast(monke, dir, 1000f, (1 << LayerMask.NameToLayer("Wall")) | (1 <<  LayerMask.NameToLayer("NoTouch")) | (1 << LayerMask.NameToLayer("Enemy")));
-       
+        Debug.Log(LayerMask.LayerToName(hit.collider.gameObject.layer));
 
         if (hit.rigidbody == null)
         {
             return;
         }
-        Debug.Log(hit.rigidbody.gameObject.name);
         if (hit.rigidbody.gameObject.layer != LayerMask.NameToLayer("Wall"))
             return;
 
