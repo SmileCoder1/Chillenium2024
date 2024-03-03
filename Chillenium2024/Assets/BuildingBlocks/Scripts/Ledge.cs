65a92727a6ddb466bf7e7c2b26ee5505993f14b8
Mega567835 <lukemason835@gmail.com>
2024-03-02 23:43:02 -0600

    spike thing

--- Chillenium2024/Assets/BuildingBlocks/Scripts/Ledge.cs
+++ Chillenium2024/Assets/BuildingBlocks/Scripts/Ledge.cs
@@ -5,11 +5,23 @@ using UnityEngine;
 public class Ledge : MonoBehaviour
 {
     public List<GameObject> untouchableEdges = new List<GameObject>();
+    public List<GameObject> spikeEdges = new List<GameObject>();
     public bool randomAssignment = false;
+    public bool randomSpike = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        if (randomSpike)
+        {
+            for(int i = 0; i < 4; i++)
+            {
+                if(Random.Range(0f, 2f) > 1f)
+                {
+                    spikeEdges[i].SetActive(true);
+                }
+            }
+        }
         if(randomAssignment)
         {
             for(int i = 0; i < 4; i++)
@@ -25,10 +37,16 @@ public class Ledge : MonoBehaviour
 
     public void setUntouchables(int ledges)
     {
+        for (int i = 0; i < spikeEdges.Count; i++)
+        {
+            bool toSet = (ledges & (1 << i)) > 0;
+            spikeEdges[i].SetActive(toSet);
+        }
         for (int i = 0; i < untouchableEdges.Count; i++)
         {
             bool toSet = (ledges & (1 << i)) > 0;
             untouchableEdges[i].SetActive(toSet);
         }
+
     }
 }
